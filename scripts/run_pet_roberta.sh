68b98facf182757c1f051f7ce2febd55bc3d6b7c
sunyilgdx@sina.com <sunyi_lgdx@sina.com>
2022-05-13 09:13:58 +0800

    1st upload

---
+++ scripts/run_pet_roberta.sh
@@ -0,0 +1,12 @@
+export PYTHONPATH=../NSP-BERT/
+for i in 1 2 3 4 5
+do
+  python ./baselines/pet_classification_roberta.py \
+  --method few-shot \
+  --n_th_set $i \
+  --device 0 \
+  --dataset_name SST-2 \
+  --batch_size 8 \
+  --learning_rate 2e-5 \
+  --model_name roberta_large
+done
